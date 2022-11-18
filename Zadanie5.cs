efd0b46e948d3a2dd4cf6d569460d5d4794a3b90
Jakub <homarofficial@gmail.com>
2022-11-18 14:37:27 +0100

    Zad5

--- Zadanie5.cs
+++ Zadanie5.cs
@@ -6,6 +6,43 @@ namespace TabliceWielowymiaroweZestawWDP1
     {
         static void Main(string[] args)
         {
+            int[,] sudoku = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                int[] userInput = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                for (int ii = 0; ii < userInput.Length; ii++)
+                {
+                    sudoku[i, ii] = userInput[ii];
+                }
+            }
+
+            /*
+            for (int i = 0; i < 9; i++)
+            {
+                for (int ii = 0; ii < 9; ii++)
+                {
+                    Console.Write("{0} ", sudoku[i, ii]);
+                }
+                Console.WriteLine();
+            }
+            */
+
+            string isCorrect = "yes";
+            for (int i = 0; i < 9; i++) //sprawdź wiersze
+            {
+                for (int ii = 0; ii < 9; ii++)
+                {
+                    for (int jj = ii; jj < 9; jj++)
+                    {
+                        if (ii != jj && (sudoku[i, ii] == sudoku[i, jj] || sudoku[ii, i] == sudoku[jj, i]))
+                        {
+                            isCorrect = "no";
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(isCorrect);
 
         }
     }
